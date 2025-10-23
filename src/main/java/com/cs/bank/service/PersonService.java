95dcdabf6e80f4eac2cd1abc1ae0fdc5c7803a11
nchandrashekar79 <test@mail.com>
2025-10-23 21:36:54 +0530

    Added repository layer

---
+++ src/main/java/com/cs/bank/service/PersonService.java
@@ -0,0 +1,32 @@
+package com.cs.bank.service;
+
+import com.cs.bank.entity.Person;
+import com.cs.bank.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    @Autowired
+    private PersonRepository personRepository;
+
+    public Person createPerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public Person getPerson(Long personId) {
+        return personRepository.findById(personId).orElse(null);
+    }
+    public Person updatePerson(Person person) {
+        return personRepository.save(person);
+    }
+    public void deletePerson(Long personId) {
+        personRepository.deleteById(personId);
+    }
+
+    public Iterable<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+
+}
