05b24cee1b750961d815a4235516a2f09a36d950
nchandrashekar79 <test@mail.com>
2025-10-23 19:44:04 +0530

    initial commit

---
+++ src/main/java/com/cs/bank/CsbankApplication.java
@@ -0,0 +1,13 @@
+package com.cs.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs/bank/CsbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
